fe42ca9e1b80b0b33a8a60fa38856377fe0ea5eb
Gonzalo Campos <erick.campos@wizeline.com>
2019-10-31 23:24:18 -0600

    Adding synchronous code

---
+++ src/main/kotlin/chapter/one/Main.kt
@@ -0,0 +1,15 @@
+package chapter.one
+
+/**
+ * Main function
+ */
+fun main(args: Array<String>) {
+
+    val o = SynchronousObservables()
+
+    // Methods found in chapter I
+    o.synchronousObservable()
+    o.inMemoryData()
+    o.synchronousComputation()
+
+}
